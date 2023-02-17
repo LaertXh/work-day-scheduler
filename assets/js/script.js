9641be343d29cab5bdaf3124e5ae79bf1985cf9a
Laert Xhumari <laertxhumari@gmail.com>
2023-02-16 21:03:29 -0500

    added funcionality to the header so it displayes the date in the appropriate format. The scheduler is now color-coded based on the current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,86 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
+
+$(document).ready(function () {
+  //DEPENDENCIES =======================================================================
+  var dateEl = $('#currentDay');
+  var timeBlockEl = $('.time-block');
+  var buttonEl = $('.fa-save');
+
+  //DATA ===============================================================================
+  var currentDate = dayjs();
+  var dateFormatted = currentDate.format('dddd, MMMM D');
+  formatDate();//correct the dateFormatted data by adding a 'st' 'nd' 'rd' or 'th' at the end
+  var currentHour = currentDate.hour();
+  
+
+  //FUNCTIONS ==========================================================================
+
+  //format the date so that it matches the mock up
+  function formatDate(){
+    if(currentDate.date() === 1){
+      dateFormatted += 'st';
+    }
+    else if(currentDate.date() === 2){
+      dateFormatted += 'nd';
+    }
+    else if(currentDate.date() === 3){
+      dateFormatted += 'rd';
+    }
+    else{
+      dateFormatted += 'th';
+    }
+  }
+  //will apply the color background to the scheduler rows based on the current hour 
+  function applyColorStyle(){
+    timeBlockEl.each(function () {
+      
+      var blockHour = this.getAttribute('id');
+      blockHour = blockHour.substring(5);
+      blockHour = parseInt(blockHour);
+
+      if(currentHour > blockHour){
+        this.setAttribute('class', 'row time-block past');
+      } 
+      else if(currentHour === blockHour){
+        this.setAttribute('class', 'row time-block present');
+      }
+      else{
+        this.setAttribute('class', 'row time-block future');
+      }
+
+
+    });
+  }
+
+  function saveButtonListener(event){
+      event.preventDefault();
+
+      var parentId = $(event.target).parent().parent().attr('id');
+      var textContent;
+
+      console.log(parentId);
+  }
+
+  //USER INTERACTIONS ==================================================================
+  //user can see the current date at the header 
+  dateEl.text(dateFormatted);
+  //user can see a color pattern on the scheduler based on the current time 
+  applyColorStyle();
+  //user can input text into the scheduler and click save which will save the text into the local storage
+  buttonEl.each(function (){
+    addEventListener('click', saveButtonListener);
+  });
+
+
+  //INITIALIZATIONS ====================================================================
+
+
+
+
+
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
